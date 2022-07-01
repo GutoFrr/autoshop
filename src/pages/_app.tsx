8f6e8f7a7c9edeadf5b89a231c1df34327f25065
Gustavo Ferreira <gustavodelferreira.12@gmail.com>
2022-07-01 09:58:47 -0300

    navbar position sticky fixe

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,15 @@ import { ThemeProvider } from 'styled-components'
 
 import GlobalStyle from '../styles/global'
 import theme from '../styles/theme'
-import Layout from '../components/layout'
 import Footer from '../components/footer'
+import Navbar from '../components/navbar'
+import Header from '../components/header'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={theme}>
-      <Layout />
+      <Header />
+      <Navbar />
       <Component {...pageProps} />
       <Footer />
       <GlobalStyle />
